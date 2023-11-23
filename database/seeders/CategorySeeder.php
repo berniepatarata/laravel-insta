f4368176e23536486bb0876c03454dfb602ec198
BerniePat <bjpatarata@gmail.com>
2023-11-23 19:00:58 +0800

    first commit

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+class CategorySeeder extends Seeder
+{
+    private $category;
+
+    public function __construct(Category $category)
+    {
+        $this->category = $category;
+    }
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+            [
+                'name'          =>  'Travel',
+                'created_at'    =>  Now(),
+                'updated_at'    =>  now()
+            ],
+
+            [
+                'name'          =>  'Food',
+                'created_at'    =>  Now(),
+                'updated_at'    =>  now()
+            ],
+
+            [
+                'name'          =>  'Lifestyle',
+                'created_at'    =>  Now(),
+                'updated_at'    =>  now()
+            ],
+
+            [
+                'name'          =>  'Music',
+                'created_at'    =>  Now(),
+                'updated_at'    =>  now()
+            ],
+
+            [
+                'name'          =>  'Career',
+                'created_at'    =>  Now(),
+                'updated_at'    =>  now()
+            ],
+
+            [
+                'name'          =>  'Movie',
+                'created_at'    =>  Now(),
+                'updated_at'    =>  now()
+            ],
+        ];
+
+        $this->category->insert($categories);
+    }
+}
